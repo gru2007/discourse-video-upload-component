11674631ec89dd8e3881bf998fb3bae8b2ea2f9c
Gru <ruslru376@gmail.com>
2024-02-01 17:06:04 +1000

    Update video.js

--- javascripts/discourse/components/modal/video.js
+++ javascripts/discourse/components/modal/video.js
@@ -81,7 +81,7 @@ export default class VideoModal extends Component {
             file: file[0],
             token: settings.vimeo_api_access_token,
             name: $("#video-title").val(),
-            description: $("#video-description").val() + '\nот @' + this.attrs.username,
+            description: $("#video-description").val() + '\nот @' + this.attrs,
             view: settings.vimeo_default_view_privacy,
             embed: settings.vimeo_default_embed_privacy,
             upgrade_to_1080: true,
